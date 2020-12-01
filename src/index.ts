0258c4e1bf32902b12c0fd8b26471bf383d74262
Maciej Żyłka <mzylka123@gmail.com>
2020-12-01 15:04:05 +0100

    added docker

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 const dbUri = (process.env.NODE_ENV === 'development' ? process.env.MONGO_URI : process.env.MONGO_URI_TEST) ?? '';
 
+console.log('env', process.env);
+
 app.use(cors());
 app.use(express.json());
 
